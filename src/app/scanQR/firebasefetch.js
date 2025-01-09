e05ea46f9a1058bf0803f6f855321c35f62c181c
Nishant_Sahni <nishantsahni04@gmail.com>
2025-01-10 03:08:23 +0530

    page.js in scanQR , its component and css dependencies , and other .js scripts

---
+++ src/app/scanQR/firebasefetch.js
@@ -0,0 +1,23 @@
+import { auth } from "@/lib/firebaseClient";
+import { onAuthStateChanged } from "firebase/auth";
+
+/**
+ * Gets the current authenticated user and calls the callback with user details.
+ * @param {Function} callback - Function to handle the authenticated user or null if no user.
+ */
+export function getCurrentUser(callback) {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in
+            callback({
+                uid: user.uid,
+                email: user.email,
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+            });
+        } else {
+            // User is signed out
+            callback(null);
+        }
+    });
+}
